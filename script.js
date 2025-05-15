ec33cbea0541dc67313a21c61d0670ab11e7ba20
oiDreas <andreasanthony92@gmail.com>
2025-05-15 18:13:29 -0300

    Add page projects

---
+++ script.js
@@ -0,0 +1,17 @@
+const imgGithubProject = document.querySelector('#imgGithubProject');
+const imgInstaProject = document.querySelector('#imgInstagramProject');
+
+imgGithubProject.addEventListener('mouseenter', () => {
+    subtitle.style.color = '#22D4FD';
+});
+imgGithubProject.addEventListener('mouseleave', () => {
+    subtitle.style.color = '#ffffff';
+});
+
+imgInstaProject.addEventListener('mouseenter', () => {
+    subtitle.style.color = '#22D4FD';
+});
+imgInstaProject.addEventListener('mouseleave', () => {
+    subtitle.style.color = '#ffffff';
+});
+
